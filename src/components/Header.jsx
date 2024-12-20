2511a2b6654eb24d0ec7e944b5eeea8ed90ae089
felipealvx <felipealvk@gmail.com>
2024-12-19 21:26:35 -0300

    adicionando download

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function Header() {
   return (
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,36 @@
 import { useState } from "react";
-import { StyleSheet, TextInput, View, TouchableOpacity, Text} from "react-native";
+import { StyleSheet, TextInput, View, TouchableOpacity, Text, Image, Platform, PermissionsAndroid} from "react-native";
 import QRCode from "react-native-qrcode-svg";
+import SvgIcon from "../../assets/icons/svg-icon.png";
+import JpgIcon from "../../assets/icons/jpg-icon.png";
+import UploadIcon from "../../assets/icons/upload-icon.png";
+// import RNFS from "react-native-fs"
 
 export default function Home() {
 
     const [text, setText] = useState('');
     const [qrCode, setQrCode] = useState('');
+
+    // const handleDownloadQRcode = async () => {
+    //     try {
+    //         if (Platform.OS === 'android') {
+    //             const granted = await PermissionsAndroid.request(
+    //                 PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+    //             )
+    //             if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
+    //                 alert('Permissões Negadas!')
+    //             }
+    //         }
+    //         await viewShotRef.current.capture().then(async (uri) => {
+    //             const path = RNFS.PicturesDirectoryPath + '/' + qrCode + '.png';
+    //             await RNFS.moveFile(uri, path);
+    //             await RNFS.scanFile(path);
+    //             alert('O QR code foi baixado!')
+    //         })
+    //     } catch (error) {
+    //         console.log (error)
+    //     }
+    // }
 
     return (
         <View style={ styles.container }>
@@ -27,14 +52,41 @@ export default function Home() {
                     style={ styles.input }
                     value={text}
                     onChangeText={setText}
+                    onChange={() => setQrCode(text)}
                 />
-                
-                <TouchableOpacity 
-                style={ styles.button }
-                onPress={() => setQrCode(text)}
-                >
-                    <Text style={ styles.buttonText }> Gerar </Text>
+
+                <TouchableOpacity
+                style={ styles.button }>
+                    <Text style={ styles.buttonText }>
+                        Inserir Logo
+                    </Text>
+                    <Image 
+                    source={UploadIcon} 
+                    style={ styles.iconStyle } />
                 </TouchableOpacity>
+                
+                <View style={ styles.saveButtons }>
+                    <TouchableOpacity 
+                    style={ styles.buttonSave }
+
+                    >
+                        <Text style={ styles.buttonText }>
+                            Baixar JPG
+                        </Text>
+                        <Image 
+                        source={JpgIcon} 
+                        style={ styles.iconStyle } />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={ styles.buttonSave }>
+                        <Text style={ styles.buttonText }>
+                            Baixar SVG
+                        </Text>
+                        <Image 
+                        source={SvgIcon} 
+                        style={ styles.iconStyle } />
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
         
@@ -45,6 +97,13 @@ const styles = StyleSheet.create ({
     container: {
         gap: 20,
         alignItems: 'center',
+    },
+    qrCode: {
+        padding: 40,
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: '#929292'
     },
     elements: {
         gap: 20,
@@ -61,18 +120,36 @@ const styles = StyleSheet.create ({
         backgroundColor: '#2F85ED',
         borderRadius: 20,
         height: 70,
-        justifyContent: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        padding: 20,
+        alignItems: 'center',
     },
     buttonText: {
-        color: 'whitesmoke',
+        color: '#fff',
         fontWeight: 600,
-        textAlign: 'center'
+        textAlign: 'center',
     },
-    qrCode: {
-        padding: 40,
-        backgroundColor: '#fff',
+    saveButtons: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        gap: 20,
+    },
+    buttonSave: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '47%',
+        backgroundColor: '#2F85ED',
+        height: 70,
         borderRadius: 20,
-        borderWidth: 1,
-        borderColor: '#929292'
-    }
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 20,
+    },
+    iconStyle: {
+        width: 25,
+        height: 25,
+
+    },
 })
